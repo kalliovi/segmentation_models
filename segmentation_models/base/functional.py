a47ec607ce60e4c7aa92de015edb3da331e113a0
kalliovi <55873258+kalliovi@users.noreply.github.com>
2021-12-08 13:54:36 +0200

    Update functional.py

--- segmentation_models/base/functional.py
+++ segmentation_models/base/functional.py
@@ -86,6 +86,9 @@ def iou_score(gt, pr, class_weights=1., class_indexes=None, smooth=SMOOTH, per_i
     backend = kwargs['backend']
 
     gt, pr = gather_channels(gt, pr, indexes=class_indexes, **kwargs)
+    max_val = backend.max(pr, axis=-1,keepdims=True)
+    cond = backend.greater_equal(pr, max_val)
+    pr = backend.switch(cond, backend.ones_like(pr),backend.zeros_like(pr))
     pr = round_if_needed(pr, threshold, **kwargs)
     axes = get_reduce_axes(per_image, **kwargs)
 
